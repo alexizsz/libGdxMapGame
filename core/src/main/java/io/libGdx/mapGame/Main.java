4caec32e1f9e5848c12a1b828d552edcb8346bc8
alexizsz <alexissalazar9@gmail.com>
2025-06-27 22:00:35 +0200

    Updated camera position

--- core/src/main/java/io/libGdx/mapGame/Main.java
+++ core/src/main/java/io/libGdx/mapGame/Main.java
@@ -2,33 +2,63 @@ package io.libGdx.mapGame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+
+    OrthographicCamera camera;
+    TiledMap map;
+    OrthogonalTiledMapRenderer mapRenderer;
+    SpriteBatch batch;
+    Texture playerTexture;
+
+    float playerX = 100;
+    float playerY = 100;
+    float playerSpeed = 100f;
 
     @Override
     public void create() {
+        map = new TmxMapLoader().load("assets/firstMap.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(map);
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        playerTexture = new Texture("assets/TX Player.png"); // put your player sprite in assets folder
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1200, 640);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        float delta = Gdx.graphics.getDeltaTime();
+
+        // Basic player movement
+        if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.W)) playerY += playerSpeed * delta;
+        if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.S)) playerY -= playerSpeed * delta;
+        if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.A)) playerX -= playerSpeed * delta;
+        if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.D)) playerX += playerSpeed * delta;
+
+        // Render map
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+
+        // Render player sprite on top of map
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(playerTexture, playerX, playerY);
         batch.end();
     }
 
     @Override
     public void dispose() {
+        map.dispose();
+        mapRenderer.dispose();
         batch.dispose();
-        image.dispose();
+        playerTexture.dispose();
     }
 }
